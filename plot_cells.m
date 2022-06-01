9142e2ba113ed48c3f0d866b73c1907f263a256d
Steven Roper <65172942+smr29git@users.noreply.github.com>
2022-06-01 10:53:46 +0100

    First commit of plot_cells.m
    Plot cells is a MATLAB function to plot the cells of a Laguerre diagram

---
+++ plot_cells.m
@@ -0,0 +1,101 @@
+function [h,id]=plot_cells(vfn,varargin)
+
+% h=plotGrains(vfn,OPTid,OPTcellcolour);
+% 
+% Plot individual grains
+%   input aguments
+%
+%   vfn      : vertex, face and neighbour data
+%   OPTid    : an optional list of cell ids in an nid x 1 vector
+%   OPTcellcolour: an optional argument that contains either a single colour for all the cells or a
+%              list of colours for each of the cells to be plotted
+%    
+%   output arguments
+%
+%   h : cell array of handles to patch objects that make up the faces of each of the plotted cells
+%       this can be used to alter the styles after the plot    
+%   id: the cell ids that were plotted
+    
+% Obtain the number of cells
+    [Nc,~]=size(vfn);
+
+    % If there are additional arguments they should refer to the colour of the faces of the cell
+    if(nargin>3)
+        error('Too many input arguments\nUsage is plot_cells(vfn), plot_cells(vfn,id) or plot_cells(vfn,id,c) where c is a colour');
+    elseif(nargin>1)
+        % Get the cell IDs to plot
+        id=varargin{1};
+
+        % Get the number of cells to plot
+        [nid,mid]=size(id);
+        if(mid>1)
+            error('The lits of cell ids should be an nid x 1 vector')
+        end
+        
+        % Check that the minimum id no less than 1 and the maximum is no greater than Nc
+        if(min(id)<1 || max(id)>Nc)
+            error('The cell ids must be in the range [1,Nc] where Nc is the number of cells')
+        end
+        
+        % Get unique cellids to be plotted
+        [id,iid]=unique(id);
+
+        if(nargin==3)
+            % Here we assume that arguments are (vfn,id,c)
+            % Get the grain colours
+            cellcolour=varargin{2};
+            [nc,~]=size(cellcolour);
+            if(nc==1)
+                cellcolour=repmat(cellcolour,nid,1);
+            elseif(nc~=nid)
+                error('The number of cells to plot and the length of the list of colours should be the same');
+            else
+                cellcolour=cellcolour(iid,:);
+            end
+        else
+            % Here we assume that the arguments are (vfn,id)
+            [nid,~]=size(id);
+            cellcolour=repmat('r',nid,1);
+        end
+    else
+        id=(1:Nc)';nid=Nc;
+        cellcolour=repmat('r',nid,1);
+    end
+
+    % Get the new number of cells to plotted (after unique)
+    [nid,~]=size(id);
+
+    % Loop through each of the cells
+    for j=1:nid,
+        cid=id(j);
+        % Can only plot non-empty cells
+        if(~isempty(vfn{cid,1}))
+            
+            % Obtain the vertices, faces and neighbours of the cell
+            verts=vfn{cid,1};
+            faces=vfn{cid,2};
+            neigh=vfn{cid,3};
+
+            % For the cell, get the number of faces
+            [Nf,~]=size(faces);
+            
+            hold on
+            for k=1:Nf,
+                fc=faces{k};
+                fv=verts(fc,:);
+                %            h{j}(i)=patchPolygon3d(fv);
+                h{j}(k)=fill3(fv(:,1),fv(:,2),fv(:,3),cellcolour(j,:));
+                %set(h{j}(k),'facecolor',graincolour(j,:));
+            end
+            hold off
+
+
+
+        end
+    end
+
+    view([-37.5,30])
+    axis equal
+
+end
+
